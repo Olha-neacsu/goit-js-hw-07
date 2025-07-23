46f8359f40df337e36250734826dc9393d43e5d1
Olha <olha4229@gmail.com>
2025-07-23 13:32:50 +0300

    3rd-task

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+// Напиши скрипт, який під час набору тексту в інпуті input#name - input(подія input) підставляє його поточне значення
+// в span#name - output як ім’я для привітання.Обов’язково очищай значення в інпуті по краях від пробілів.Якщо інпут порожній
+// або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous".
+
+const inputIt = document.querySelector("#name-input");
+const outputIt = document.querySelector("#name-output");
+
+inputIt.addEventListener("input", () => {
+    const trimmedValue = inputIt.ariaValueMax.trim();
+    outputIt.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
+});
